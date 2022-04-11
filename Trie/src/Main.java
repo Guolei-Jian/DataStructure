7be24712e5274baa66a7a60c0f8da869b1ba8560
Jane <guoleijian@gmail.com>
2022-04-11 15:56:16 +0800

    Add Trie

---
+++ Trie/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+
+/*
+Trie是多叉树
+通常Trie只用来处理字符串
+查询每个条目的时间复杂度与存储了多少条目无关，时间复杂度为O(W)，W为查询单词的长度
+Trie中的每个节点有若干个指向下个节点的指针
+ */
