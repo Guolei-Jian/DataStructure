a627ef51bcf537b1a676e854753d4740343033f0
Jane <guoleijian@gmail.com>
2022-04-14 13:34:14 +0800

    Add AVL

---
+++ AVL/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+/*
+平衡二叉树：对于任意一个节点，左子树和右子树的高度差不能超过1
+标注节点的高度，从叶子节点开始，叶子节点为1
+平衡因子：左右子树的高度差
+
+AVL树在什么时候维护平衡？加入节点后，沿着节点向上维护平衡性
+ */
